f1aefc16591fb31876f2b362e06c90ad0e567bd9
Saeed-Ali <msa0394@gmail.com>
2021-08-19 10:50:03 +0100

    modified the schema

---
+++ Schema/cinema_schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE if exists cinema;
+CREATE DATABASE cinema;
+USE DATABASE cinema;
+
+CREATE TABLE films (
+	film_id INT AUTO_INCREMENT,
+	film_name VARCHAR(50) NOT NULL,
+	film_duration TIME NOT NULL,
+	screening_time DATETIME NOT NULL,
+	PRIMARY KEY (film_id)
+);
+
+CREATE TABLE customers (
+	customer_id INT AUTO_INCREMENT,
+	customer_name VARCHAR(40) NOT NULL,
+	customer_email VARCHAR(50) UNIQUE,
+	customer_age TINYINT NOT NULL,
+	PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE orders (
+	order_id INT AUTO_INCREMENT,
+	fk_film_id INT, 
+	fk_customer_id INT,
+	date_of_booking DATETIME NOT NULL,
+	PRIMARY KEY (order_id),
+	FOREIGN KEY (fk_film_id) REFERENCES films(film_id),
+	FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id)
+);
