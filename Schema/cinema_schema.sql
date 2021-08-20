d5d29adb9e080cf997278fb18d759099106c9d07
Saeed-Ali <msa0394@gmail.com>
2021-08-20 10:21:59 +0100

    updated film table

--- Schema/cinema_schema.sql
+++ Schema/cinema_schema.sql
@@ -6,15 +6,15 @@ CREATE TABLE films (
 	film_id INT AUTO_INCREMENT,
 	film_name VARCHAR(50) NOT NULL,
 	film_duration INT NOT NULL,
-	screening_time DATETIME NOT NULL,
+	film_genre VARCHAR(20) NOT NULL,
 	PRIMARY KEY (film_id)
 );
 
 CREATE TABLE customers (
 	customer_id INT AUTO_INCREMENT,
 	customer_name VARCHAR(40) NOT NULL,
+	customer_age TINYINT NOT NULL
 	customer_email VARCHAR(50) UNIQUE,
-	customer_age TINYINT NOT NULL,
 	PRIMARY KEY (customer_id)
 );
 
